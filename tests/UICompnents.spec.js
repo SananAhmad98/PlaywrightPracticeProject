e72b123e524d9f18aa1fa319626c16e7708fcde1
SananAhmad98 <sananahmad98@gmail.com>
2025-10-10 18:17:08 +0500

    Included handling of child tabs

--- tests/UICompnents.spec.js
+++ tests/UICompnents.spec.js
@@ -1,16 +1,18 @@
 const {test,expect} = require('@playwright/test');
 
-test.only("Static dropdown Test", async ({page,browser}) =>{
 
+test("Static dropdown Test", async ({page,browser}) =>{
+
+    //main page locators
     const userName = page.locator('#username');
     const password = page.locator("[type='password']");
     const signInBtn = page.locator('#signInBtn');
     const dropdown = page.locator('select.form-control');
     const RB = page.locator('.radiotextsty')
     const termsCB = page.locator('#terms');
+    const freeAccessLink = page.locator("[href*='documents-request']");
     const okBtn = page.locator('#okayBtn');
-    
-    
+
     //Static dropdown
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     await userName.fill("rahulshettyacademy");
@@ -31,4 +33,40 @@ test.only("Static dropdown Test", async ({page,browser}) =>{
     await expect(termsCB).toBeChecked();
     await termsCB.uncheck();
     expect(await termsCB.isChecked()).toBeFalsy()
+
+    //Blinking link
+    await expect(freeAccessLink).toHaveAttribute("class","blinkingText");
 });
+
+test.only("Child Windows and Tabs test", async({browser}) => {
+
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    //main page locators
+    const userName = page.locator('#username');
+    const password = page.locator("[type='password']");
+    const signInBtn = page.locator('#signInBtn');
+    const dropdown = page.locator('select.form-control');
+    const RB = page.locator('.radiotextsty')
+    const termsCB = page.locator('#terms');
+    const freeAccessLink = page.locator("[href*='documents-request']");
+    const okBtn = page.locator('#okayBtn');
+
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+    //Following line helps us to make sure that both steps execute asynchronously and then we would be able to work on a new tab.
+    //newPage is wrapped up in brackets as action can open/return multiple tabs.
+    const [newPage]  = await Promise.all([context.waitForEvent("page"),freeAccessLink.click()]);
+    const textOfRedPara = await newPage.locator(".red").textContent();
+
+    const mainTextRedParaArray = textOfRedPara.split("@");
+    const domainName = mainTextRedParaArray[1].split(" ")[0];
+    console.log(domainName);
+
+    await userName.fill(domainName);
+    await page.pause();
+    console.log (await userName.textContent());
+
+});
+

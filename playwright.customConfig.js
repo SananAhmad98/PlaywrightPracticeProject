cfe49976e99002736d7281d7d758a2393e6eee8e
SananAhmad98 <sananahmad98@gmail.com>
2025-10-21 19:00:07 +0500

    Included parallel and serial mode of test execution using workers.

--- playwright.customConfig.js
+++ playwright.customConfig.js
@@ -8,6 +8,8 @@ const config = ({
 
   testDir: './tests',
   timeout: 30 * 1000, //This timeout will be utilized for normal execution of statements. Default timeout is 30 seconds.
+  retries: 1, //This will retry the execution of test case if it is failed on the first attempt.
+  workers: 3, //This option will be utilized to run test files in a parallel mode.
   expect: { //This expect timeout will be applied on the assertions
 
     timeout: 5000,
@@ -22,12 +24,12 @@ const config = ({
       use: {
 
         browserName: 'webkit', //chromium, firefox, and webkit
-        headless: true,
+        headless: false,
         //this will take screenshot for all the actions throughout the script.
         screenshot: 'on', //it can be off, on, retain-on-failure
         //this will trace each action and generate logs for further debugging
         trace: 'on', //it can be off, on, retain-on-failure
-        ...devices['iPhone 11']
+        //...devices['iPhone 11']
 
       }
     },
@@ -42,10 +44,11 @@ const config = ({
         screenshot: 'off', //it can be off, on, retain-on-failure
         //this will trace each action and generate logs for further debugging
         trace: 'off', //it can be off, on, retain-on-failure
+        video: 'retain-on-failure', //on, off,
         ignoreHttpsErrors: true, //This will bypass SSL certificates without validation.
-        permissions:['geolocation'],
+        permissions:['geolocation'], //bypass location pop-up
         //viewport: { width: 1000, height: 1000 }, //for customized dimensions
-        ...devices['Galaxy S24'],
+        //...devices['Galaxy S24'], //test will fail as the web app is not user freindly for Glaxy S24
       }
     }
   ],
---
+++ tests/13-E2ETestsWithParallelExecution.spec.js
@@ -0,0 +1,94 @@
+const {test,expect} = require('@playwright/test');
+const {BasePage} = require("../pageobjects/BasePage");
+const {customTest} = require("../utils/CustomFixtureForE2ETestWithCF");
+
+//This will utilize the number of workers configured in custom config file to execute tests in parallel as per requirement.
+test.describe.configure({mode:"parallel"});
+
+customTest("E2E Testcase to dynamically select products", async({browser,testDataForLoginPage,testDataForDashboardPage,testDataForOrderPage}) => {
+
+const context = await browser.newContext();
+const page = await context.newPage();
+
+//Base Page object
+const basePage = new BasePage(testDataForDashboardPage.productName,page);
+
+//Go to login Page
+const loginPage = await basePage.getLoginPage();
+await loginPage.landOnLoginPage("https://rahulshettyacademy.com/client/");
+await loginPage.validLogin(testDataForLoginPage.email,testDataForLoginPage.password);
+
+//Dashboard/products page actions
+const dashboardPage = await basePage.getDashboardPage();
+await dashboardPage.selectDesiredProduct();
+await dashboardPage.navigateToAddToCart();
+
+//Cart page actions
+//we are waiting here because there is no automatic waiting mechanism for isVisible().
+const cartPage = await basePage.getCartPage();
+await cartPage.waitForCartItems(); 
+const isProductAvailableInCart = await cartPage.isActualProductAvailableInCart();
+await expect(isProductAvailableInCart).toBeTruthy();
+await cartPage.clickCheckOutButton();
+
+//Order page actions
+const orderPage = await basePage.getOrderPage();
+await orderPage.provideCardInformation(testDataForOrderPage.cvv,testDataForOrderPage.nameOnCard);
+const appliedCoupon = await orderPage.applyAndValidateCoupon(testDataForOrderPage.couponValue);
+await expect(appliedCoupon).toHaveText("* Coupon Applied");
+
+//handling dynamic dropdown
+await orderPage.selectCountry(testDataForOrderPage.countryName);
+const orderEmail = await orderPage.validateLoginEmailOnOrderPage();
+await expect(orderEmail).toHaveText(testDataForLoginPage.email);
+await orderPage.placeOrder();
+
+//Thanks Page actions
+const orderConfirmationPage = await basePage.getOrderConfirmationPage();
+await expect(await orderConfirmationPage.validateOrderConfirmation()).toHaveText(" Thankyou for the order. ");
+const orderID = await orderConfirmationPage.getOrderID();
+await orderConfirmationPage.navigateToOrderHistoryPage();
+
+//order history page actions
+const orderHistoryPage = await basePage.getOrderHistoryPage();
+await orderHistoryPage.validateOrderInOrderHistory(orderID);
+
+//order details page actions
+const orderDetailsPage = await basePage.getOrderDetailsPage();
+await expect (await orderDetailsPage.validateOrderDetails()).toHaveText("Thank you for Shopping With Us");
+
+});
+
+customTest("E2E Testcase to apply coupon", async({browser,testDataForLoginPage,testDataForDashboardPage,testDataForOrderPage}) => {
+
+const context = await browser.newContext();
+const page = await context.newPage();
+
+//Base Page object
+const basePage = new BasePage(testDataForDashboardPage.productName,page);
+
+//Go to login Page
+const loginPage = await basePage.getLoginPage();
+await loginPage.landOnLoginPage("https://rahulshettyacademy.com/client/");
+await loginPage.validLogin(testDataForLoginPage.email,testDataForLoginPage.password);
+
+//Dashboard/products page actions
+const dashboardPage = await basePage.getDashboardPage();
+await dashboardPage.selectDesiredProduct();
+await dashboardPage.navigateToAddToCart();
+
+//Cart page actions
+//we are waiting here because there is no automatic waiting mechanism for isVisible().
+const cartPage = await basePage.getCartPage();
+await cartPage.waitForCartItems(); 
+const isProductAvailableInCart = await cartPage.isActualProductAvailableInCart();
+await expect(isProductAvailableInCart).toBeTruthy();
+await cartPage.clickCheckOutButton();
+
+//Order page actions
+const orderPage = await basePage.getOrderPage();
+await orderPage.provideCardInformation(testDataForOrderPage.cvv,testDataForOrderPage.nameOnCard);
+const appliedCoupon = await orderPage.applyAndValidateCoupon(testDataForOrderPage.couponValue);
+await expect(appliedCoupon).toHaveText("* Coupon Applied");
+
+});
